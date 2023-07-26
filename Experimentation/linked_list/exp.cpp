1333443cdc1c3db921136c7cf0fe80f428cc1bbd
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-07-26 14:50:13 -0300

    Insertion samples

--- Experimentation/linked_list/exp.cpp
+++ Experimentation/linked_list/exp.cpp
@@ -4,49 +4,97 @@
 #include<chrono>
 #include<string>
 #include<vector>
+#include<list>
+#include<math.h>
 
 using namespace std;
 
-List<int> list;
-vector<int> v;
+vector<int> test;
 
-void read_input() {
+void read_input(string filepath, List<int>& l) {
     int number;
     ifstream inputFile;
-    string filePath = "../inputs/input_2";
-    inputFile.open(filePath);
+    inputFile.open(filepath);
     while (inputFile) {
         inputFile >> number;
-        list.push_back(number);
-        v.push_back(number);
+        l.push_back(number);
     }
     inputFile.close();
 }
 
-void writeData(long long d, int t) {
+void writeData(long long d, int t, string filename) {
     ofstream outputFile;
-    outputFile.open("runtime.csv",ios::app);
+    outputFile.open(filename,ios::app);
     outputFile << "\n" << to_string(t) << "," << to_string(d);
     outputFile.close();
 }
 
-int main() {
-    int c = 19;
-    int i = 0;
-    read_input();
+void sample_insertion() {
+    for(int i = 20; i < 27; i++) {  
+        int k = 0;
+        int j = pow(2,i);
+        vector<int> v;
+        List<int> l1;
+        List<int> l2;
+        list<int> lc;
 
-    while (c--) {
-        int j = 10000 * (i+1);
+        cout << "Testing with size: 2^" << i << endl;
+
+        cout << "My list" << endl;
 
         auto start = chrono::high_resolution_clock::now();
-
-        int num = list[j];
-
+        while(k <= j) {
+            l1.push_front(k);
+            k++;
+        }
         auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        writeData(duration.count(), j, "samples/linked_push_front.csv");
 
-        auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+        k = 0;
+        start = chrono::high_resolution_clock::now();
+        while(k <= j) {
+            l2.push_back(k);
+            k++;
+        }
+        stop = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        writeData(duration.count(), j, "samples/linked_push_back.csv");
 
-        writeData(duration.count(), j);
-        i++;
+        cout << "Vector" << endl;
+        k = 0;
+        start = chrono::high_resolution_clock::now();
+        while(k <= j) {
+            v.push_back(k);
+            k++;
+        }
+        stop = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        writeData(duration.count(), j, "../samples_cpp/vector_push_back.csv");
+
+        cout << "C++ list" << endl;
+        k = 0;
+        start = chrono::high_resolution_clock::now();
+        while(k <= j) {
+            lc.push_back(k);
+            k++;
+        }
+        stop = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        writeData(duration.count(), j, "../samples_cpp/list_push_back.csv");
+
+        k = 0;
+        start = chrono::high_resolution_clock::now();
+        while(k <= j) {
+            lc.push_front(k);
+            k++;
+        }
+        stop = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        writeData(duration.count(), j, "../samples_cpp/list_push_front.csv");
     }
+}
+
+int main() {
+    
 }
--- Experimentation/linked_list/plot.py
+++ Experimentation/linked_list/plot.py
@@ -3,16 +3,45 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("runtime.csv")
+data_pb_l = pd.read_csv("samples/linked_push_back.csv")
+data_pf_l = pd.read_csv("samples/linked_push_front.csv")
+data_push_v = pd.read_csv("../samples_cpp/vector_push_back.csv")
+data_pb_lc = pd.read_csv("../samples_cpp/list_push_back.csv")
+data_pf_lc = pd.read_csv("../samples_cpp/list_push_front.csv")
 
-rate = np.polyfit(data['n'], data['time'], 1)
-trend_line = np.polyval(rate, data['n'])
+fig1 = plt.figure(figsize=(8, 6))
 
-plt.plot(data['n'], data['time'], linewidth = 2.5, label = 'Time')
-plt.plot(data['n'], trend_line, color = 'red', linewidth = 1.5, label = 'Tendency')
-
+plt.plot(data_pb_lc['n'], data_pb_lc['time'], label='C++ List push back', color='red', linewidth = 2)
+plt.plot(data_pf_lc['n'], data_pf_lc['time'], label='C++ List push front', color='orange', linewidth = 2)
+plt.plot(data_pb_l['n'], data_pb_l['time'], label='List push back', color='blue', linewidth = 2)
+plt.plot(data_pf_l['n'], data_pf_l['time'], label='List push front', color='green', linewidth = 2)
 plt.xlabel('N')
-plt.ylabel('Time (microseconds)')
-plt.title('Linked list access time')
+plt.ylabel('Time (miliseconds)')
+plt.title('Insertion Double Linked List vs C++ List')
+plt.suptitle('Insertion comparison with C++ list')
+plt.grid(True)
 plt.legend()
 plt.show()
+
+fig2 = plt.figure(figsize=(8, 6))
+
+plt.plot(data_push_v['n'], data_push_v['time'], label='C++ Vector', color='red', linewidth = 2)
+plt.plot(data_pb_l['n'], data_pb_l['time'], label='List push back', color='blue', linewidth = 2)
+plt.plot(data_pf_l['n'], data_pf_l['time'], label='List push front', color='green', linewidth = 2)
+plt.xlabel('N')
+plt.ylabel('Time (miliseconds)')
+plt.title('Insertion Double Linked List vs C++ bector')
+plt.suptitle('Insertion comparison with C++ vector')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+fig3 = plt.figure(figsize=(8, 6))
+
+
+
+
+
+
+
+
