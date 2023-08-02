92e6ce43e14603251669c112807b1fd4065712b5
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-08-02 13:47:55 -0300

    Typo

--- binary search tree/tests.cpp
+++ binary search tree/tests.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 string accepted = "accpeted";
-string wrong    = "wrong answer";
+string wrong    = "wrong anwser";
 int testCases   = 0;
 int acceptedC   = 0;
 
@@ -18,15 +18,15 @@ void print_judge(string text, bool a) {
 
 void test_empty_constructor() {
     bst<int> a;
-    bool answer = a.size() == 0;
-    print_judge("Empty constructor",answer);
+    bool anwser = a.size() == 0;
+    print_judge("Empty constructor",anwser);
 }
 
 void test_one_insert() {
     bst<int> a;
     a.insert(37);
-    bool answer = a.size() == 1;
-    print_judge("One insert",answer);
+    bool anwser = a.size() == 1;
+    print_judge("One insert",anwser);
 }
 
 void test_some_inserts() {
@@ -36,70 +36,70 @@ void test_some_inserts() {
     a.insert(1527);
     a.insert(3);
     a.insert(999);
-    bool answer = a.size() == 5;
-    print_judge("Some inserts",answer);
+    bool anwser = a.size() == 5;
+    print_judge("Some inserts",anwser);
 }
 
 void test_contains() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
 
     for (int i = 0; i < 0;i++) {
-        if (a.contains(i)) answer = false;
+        if (a.contains(i)) anwser = false;
     }
 
     a.insert(10);
-    if (!a.contains(10)) answer = false;
+    if (!a.contains(10)) anwser = false;
 
     a.insert(88);
-    if (!a.contains(10)) answer = false;
-    if (!a.contains(88)) answer = false;
+    if (!a.contains(10)) anwser = false;
+    if (!a.contains(88)) anwser = false;
 
-    print_judge("Contains", answer);
+    print_judge("Contains", anwser);
 }
 
 void test_insert_remove() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
 
     a.insert(153);
     a.remove(153);
-    if (a.size() != 0) answer = false;
+    if (a.size() != 0) anwser = false;
 
     a.insert(35);
-    if (a.size() != 1 and !a.contains(35)) answer = false;
+    if (a.size() != 1 and !a.contains(35)) anwser = false;
 
-    print_judge("Insert & remove", answer);
+    print_judge("Insert & remove", anwser);
 }
 
 void test_min() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
     a.insert(23);
     a.insert(21);
     a.insert(58);
     a.insert(152);
-    if(a.min() != 21) answer = false;
-    print_judge("Min", answer);
+    if(a.min() != 21) anwser = false;
+    print_judge("Min", anwser);
 }
 
 void test_max() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
     a.insert(1);
     a.insert(21);
     a.insert(58);
     a.insert(152);
-    if (!a.contains(1)) answer = false;
-    if (!a.contains(21)) answer = false;
-    if (!a.contains(58)) answer = false;
-    if (!a.contains(152)) answer = false;
-    if(a.max() != 152) answer = false;
-    print_judge("Max", answer);
+    if (!a.contains(1)) anwser = false;
+    if (!a.contains(21)) anwser = false;
+    if (!a.contains(58)) anwser = false;
+    if (!a.contains(152)) anwser = false;
+    if(a.max() != 152) anwser = false;
+    print_judge("Max", anwser);
 }
 
 void test_next() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
     a.insert(5);
     a.insert(4);
@@ -110,40 +110,40 @@ void test_next() {
     a.insert(24);
     a.insert(22);
     a.insert(25);
-    if (a.next(20) != 22) answer = false;
-    if (a.next(4) != 5) answer = false;
-    if (a.next(15) != 16) answer = false;
-    print_judge("Next number order",answer);
+    if (a.next(20) != 22) anwser = false;
+    if (a.next(4) != 5) anwser = false;
+    if (a.next(15) != 16) anwser = false;
+    print_judge("Next number order",anwser);
 }
 
 void test_remove_leaf() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
     a.insert(1);
     a.insert(21);
     a.insert(58);
     a.insert(152);
     a.remove(152);
-    if(a.size() != 3) answer = false;
-    if(a.contains(152)) answer = false;
-    print_judge("Remove one leaf",answer);
+    if(a.size() != 3) anwser = false;
+    if(a.contains(152)) anwser = false;
+    print_judge("Remove one leaf",anwser);
 }
 
 void test_remove_one_son() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
     a.insert(1);
     a.insert(21);
     a.insert(58);
     a.insert(152);
     a.remove(58);
-    if(a.size() != 3) answer = false;
-    if(a.contains(58)) answer = false;
-    print_judge("Remove with one son",answer);
+    if(a.size() != 3) anwser = false;
+    if(a.contains(58)) anwser = false;
+    print_judge("Remove with one son",anwser);
 }
 
 void test_remove_both_sons() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
     a.insert(5);
     a.insert(4);
@@ -155,9 +155,9 @@ void test_remove_both_sons() {
     a.insert(22);
     a.insert(25);
     a.remove(20);
-    if(a.size() != 8) answer = false;
-    if(a.contains(20)) answer = false;
-    print_judge("Remove with two sons",answer);
+    if(a.size() != 8) anwser = false;
+    if(a.contains(20)) anwser = false;
+    print_judge("Remove with two sons",anwser);
 }
 
 const int total = 1000;
@@ -167,58 +167,58 @@ int key(int i) {
 }
 
 void test_stress() {
-    bool answer = true;
+    bool anwser = true;
     bst<int> a;
 
     // Insert
     for (int i = 0; i < total; i++) {
 	    int k = key(i);
-	    if (a.size() != i) answer = false;
-        if (a.contains(k)) answer = false;
+	    if (a.size() != i) anwser = false;
+        if (a.contains(k)) anwser = false;
 	    a.insert(k);
-	    if (!a.contains(k)) answer = false;
+	    if (!a.contains(k)) anwser = false;
     }
-    if (a.size() != total) answer = false;
+    if (a.size() != total) anwser = false;
 
     // Insert again
     for (int i = 0; i < total; i++) {
 	    int k = key(i);
-        if (!a.contains(k)) answer = false;
+        if (!a.contains(k)) anwser = false;
 	    a.insert(k);
-	    if (!a.contains(k)) answer = false;
-        if (a.size() != total) answer = false;
+	    if (!a.contains(k)) anwser = false;
+        if (a.size() != total) anwser = false;
     }
 
     // Remove number for even i
     for (int i = 0; i < total; i++) {
 	    int k = key(i);
-	    if (!a.contains(k)) answer = false;
+	    if (!a.contains(k)) anwser = false;
 	    if (i % 2 == 0) {
 	    	a.remove(k);
-	    	if (a.contains(k)) answer = false;
+	    	if (a.contains(k)) anwser = false;
 	    }
     }
-    if (a.size() != total / 2) answer = false;
+    if (a.size() != total / 2) anwser = false;
 
     // Remove number for odd i
     for (int i = 0; i < total; i++) {
 	    int k = key(i);
 	    if (i % 2 == 1) {
-            if (!a.contains(k)) answer = false;
+            if (!a.contains(k)) anwser = false;
 	    	a.remove(k);
-	    	if (a.contains(k)) answer = false;
+	    	if (a.contains(k)) anwser = false;
 	    } else {
-            if (a.contains(k)) answer = false;
+            if (a.contains(k)) anwser = false;
         }
     }
-    if (a.size() != 0) answer = false;
+    if (a.size() != 0) anwser = false;
 
     for(int i = 0; i < total; i++) {
         int k = key(i);
-        if (a.contains(k)) answer = false;
+        if (a.contains(k)) anwser = false;
     }
 
-    print_judge("Stress",answer);
+    print_judge("Stress",anwser);
 }
 
 int main() {
