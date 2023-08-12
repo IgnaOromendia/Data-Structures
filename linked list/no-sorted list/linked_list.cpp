6220e25ad8159abae5b39112f06bf95b81c9177b
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-08-12 18:27:03 -0300

    Non sorted list

---
+++ linked list/no-sorted list/linked_list.cpp
@@ -0,0 +1,154 @@
+#include "linked_list.h"
+
+template <typename T>
+List<T>::Node::Node(const T& elem): value(elem), next(nullptr), prev(nullptr) {};
+
+template <typename T>
+List<T>::List(): _first(nullptr), _last(nullptr), _length(0) {};
+
+template <typename T>
+List<T>::List(const List<T> &l){
+    *this = l;
+};
+
+template <typename T>
+List<T>::~List(){
+    delete_all();
+};
+
+template <typename T>
+List<T>& List<T>::operator=(const List<T> &to_copy){
+    this->_length = 0;
+    for(int i = 0; i < to_copy.length(); i++) {
+        T elem = to_copy[i];
+        this->push_back(elem);
+    }
+
+    return *this;
+}
+
+template <typename T>
+const T &List<T>::operator[](int i) const{
+    Node* it = _first;
+    if (i < _length) {
+        if (i > _length / 2) {
+            it = _last;
+            for(int j = _length - 1; j > i; j--) {
+                it = it->prev;
+            }
+        } else {
+            for(int j = 0; j < i; j++) {
+                it = it->next;
+            }
+        }
+    }
+    return it->value;
+};
+
+template <typename T>
+void List<T>::push_front(const T& elem){
+    Node* new_node = new Node(elem);
+    new_node->next = _first;
+    if(_length > 0) {
+        _first->prev = new_node;
+    } else {
+        _last = new_node;
+    }
+    _first = new_node;
+    _length++;
+}
+
+template <typename T>
+void List<T>::push_back(const T &elem){
+    Node* new_node = new Node(elem);
+    if (_length == 1) {
+        _last = new_node;
+        _last->prev = _first;
+        _first->next = _last;
+    } else if(_length > 1) {
+        _last->next = new_node;
+        new_node->prev = _last;
+        _last = new_node;
+    } else {
+        _first = new_node;
+        _last = new_node;
+    }
+    _length++;
+};
+
+template <typename T>
+void List<T>::remove_at(int i){
+    Node* it = _first;
+    if (i < _length) {
+        for(int j = 0; j < i; j++) {
+            it = it->next;
+        }
+
+        if(it == _first) {
+            if (_length == 1) {
+                _first = nullptr;
+                _last = nullptr;
+            } else {
+                _first = it->next;
+            }
+        } else if (it == _last) {
+            _last = it->prev;
+            it->prev->next = nullptr;
+        } else {
+            it->prev->next = it->next;
+            it->next->prev = it->prev;
+        }
+
+        delete it;
+        _length--;
+    }
+}
+
+template <typename T>
+void List<T>::remove(T elem){
+    Node* it = _first;
+    for (int i = 0; i < _length; i++) {
+        if(it->value != elem) continue;
+        this->remove_at(i);
+    }
+}
+
+template <typename T>
+int List<T>::length() const {
+    return _length;
+}
+
+template <typename T>
+void List<T>::merge(const List<T>& l){
+    for(int i = 0; i < l._length; i++) {
+        push_back(l[i]);
+    }
+};
+
+template <typename T>
+void List<T>::print(ostream &os) {
+    Node* it = _first;
+    os << "[";
+    while (it != nullptr) {
+        if (it != _last) {
+            os << it->value << ", ";
+        } else {
+            os << it->value;
+        }
+        it = it->next;
+    }
+    os << "]";
+}
+
+// AUXILIARS
+
+template <typename T>
+void List<T>::delete_all() {
+    while(_first != nullptr) {
+        Node* f = _first;
+        _first->prev = nullptr;
+        _first = _first->next;
+        delete f;
+    }
+    _length = 0;
+}
---
+++ linked list/no-sorted list/linked_list.h
@@ -0,0 +1,57 @@
+#ifndef _list_h
+#define _list_h
+
+#include <ostream>
+
+using namespace std;
+
+template<typename T>
+class List {
+public:
+
+    List();
+
+    List(const List<T>& l);
+
+    ~List();
+
+    List<T>& operator=(const List<T>& to_copy);
+
+    const T& operator[](int i) const;
+
+    void push_front(const T& elem);
+
+    void push_back(const T& elem);
+
+    void remove_at(int i);
+
+    void remove(T elem);
+
+    int length() const;
+
+    void merge(const List<T>& l);
+
+    void print(ostream& os);
+
+    friend ostream& operator<<(ostream& os, List<T>& l) {
+        l.print(os);
+        return os;
+    }
+
+private:
+
+    struct Node {
+        Node(const T& elem);
+        T value;
+        Node* next;
+        Node* prev;
+    };
+
+    Node* _first;
+    Node* _last;
+    int _length;
+
+    void delete_all();
+};
+
+#endif
